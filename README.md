1585ede95207ab13293ce563af22c6aa03cf60cc
amna <amna.ejaz488@gmail.com>
2025-01-08 03:03:52 -0500

    Initial commit with README generator

---
+++ README.md
@@ -0,0 +1,40 @@
+# Professtional README Generator
+  [![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+  ## Description
+  
+  This App Generates a README file based on user's response.
+  
+  ## Table of Contents
+  
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Contributing](#contributing)
+  - [Tests](#tests)
+  - [Questions](#questions)
+  
+  ## Installation
+  
+  Please make sure to install Node and Inquierer package.
+  
+  ## Usage
+  
+  Use as described in license terms and conditions.
+  
+  ## License
+
+[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)
+
+This project is licensed under the [MIT](https://opensource.org/licenses/MIT) license.
+  
+  ## Contributing
+  
+  Give Credit to Authers.
+  
+  ## Tests
+  
+  Use command: Node index.js,to execute program.
+  ## Questions
+
+If you have any questions about the project, please open an issue or contact me directly at amna.ejaz488@gmail.com. You can find more of my work at [Amnaejaz488](https://github.com/Amnaejaz488).
---
+++ index.js
@@ -0,0 +1,34 @@
+import inquirer from 'inquirer';
+import fs from 'fs';
+import generateMarkdown from './utils/generateMarkdown.js';
+
+// Array of questions for user input
+const questions = [
+    { type: 'input', name: 'title', message: 'Add title of project' },
+    { type: 'input', name: 'description', message: 'Add description of project' },
+    { type: 'input', name: 'installation', message: 'Add installation instructions' },
+    { type: 'input', name: 'usage', message: 'Add usage information' },
+    { type: 'list', name: 'license', message: 'Under which license is your application covered?', choices: ['MIT', 'Apache 2.0', 'None'] },
+    { type: 'input', name: 'contributing', message: 'Add contribution guidelines' },
+    { type: 'input', name: 'tests', message: 'Add test instructions' },
+    { type: 'input', name: 'github', message: 'Enter your GitHub username' },
+    { type: 'input', name: 'email', message: 'Enter your email address' },
+];
+
+// Function to write README file
+function writeToFile(fileName, data) {
+    fs.writeFile(fileName, data, (err) =>
+        err ? console.log('Error:', err) : console.log(`${fileName} has been successfully created!`)
+    );
+}
+
+// Function to initialize app
+function init() {
+    inquirer.prompt(questions).then((answers) => {
+        console.log("Generating README...");
+        writeToFile("README.md", generateMarkdown({ ...answers }));
+    });
+}
+
+// Function call to initialize app
+init();
